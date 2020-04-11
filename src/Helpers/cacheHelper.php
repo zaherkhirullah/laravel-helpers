b56bec9d4bcb209892b9f13897e4dfcb4dd8e6f4
zahir hayrullah <z.hayrullah@imtilak.net>
2020-04-11 12:47:50 +0300

    add Cache Helper

---
+++ src/Helpers/cacheHelper.php
@@ -0,0 +1,112 @@
+<?php
+
+use Illuminate\Support\Str;
+
+if (!function_exists('forget_cache')) {
+    /**
+     * @param $key
+     *
+     * @throws Exception
+     */
+    function forget_cache($key)
+    {
+        // delete this query from cache
+        cache()->forget($key);
+    }
+}
+/*--------------------------------------{</>}----------------------------------------*/
+
+if (!function_exists('get_cache')) {
+    /**
+     * @param $key
+     *
+     * @throws Exception
+     * @return mixed
+     */
+    function get_cache($key)
+    {
+        return cache()->get($key);
+    }
+}
+/*--------------------------------------{</>}----------------------------------------*/
+
+if (!function_exists('refresh_cache')) {
+    /**
+     * @param $key
+     * @param $query
+     *
+     * @throws Exception
+     * @return bool
+     */
+    function refresh_cache($key, $query)
+    {
+        // delete this query from cache
+        cache()->forget($key);
+
+        // caching again
+        cache()->rememberForever($key, function () use ($query) {
+            return $query;
+        });
+
+        return true;
+    }
+}
+/*--------------------------------------{</>}----------------------------------------*/
+
+if (!function_exists('get_from_cache_by_slug')) {
+    /**
+     * @param        $slug
+     * @param string $model
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     * @return mixed
+     */
+    function get_from_cache_by_slug($slug, $model = "App\\Page")
+    {
+
+        $key = getCacheKey($slug, $model);
+        // caching project if not existing
+        if (!cache()->has($key)) {
+            cache()->rememberForever($key, function () use ($model, $slug) {
+                return $model::active()->where('slug', $slug)->firstOrFail();
+            });
+        }
+
+        // get page from cache
+        return get_cache($key);
+    }
+}
+/*--------------------------------------{</>}----------------------------------------*/
+
+if (!function_exists('get_cache_key')) {
+    /**
+     * @param $slug
+     * @param $className
+     *
+     * @return mixed
+     */
+    function get_cache_key($slug, $className = null)
+    {
+        $name = get_model_name_by_class($className);
+
+        return "{$name}_{$slug}"; // page_slug
+    }
+}
+/*--------------------------------------{</>}----------------------------------------*/
+
+if (!function_exists('get_model_name_by_class')) {
+    /**
+     * @param        $className
+     * @param string $namespace
+     *
+     * @return mixed
+     */
+    function get_model_name_by_class($className = null, $namespace = 'App\\')
+    {
+        $name = str_replace($namespace, '', $className);
+        $name = Str::lower($name);
+
+        return $name;
+    }
+}
+/*--------------------------------------{</>}----------------------------------------*/
--- src/Helpers/helper.php
+++ src/Helpers/helper.php
@@ -29,3 +29,45 @@ if (!function_exists('get_item_if_exists')) {
     }
 }
 /*---------------------------------- </> --------------------------------*/
+
+if (!function_exists('list_of_models')) {
+    /**
+     * @return array
+     */
+    function list_of_models()
+    {
+        return [
+            ['name' => 'permission-groups', 'controller' => 'PermissionGroupController', 'except' => null, 'only' => null, 'model' => '\\App\\PermissionGroup'],
+            ['name' => 'permissions', 'controller' => 'PermissionController', 'except' => null, 'only' => null, 'model' => '\\App\\Permission'],
+            ['name' => 'users', 'controller' => 'UserController', 'except' => null, 'only' => null, 'model' => '\\App\\User'],
+            ['name' => 'roles', 'controller' => 'RoleController', 'except' => null, 'only' => null, 'model' => '\\App\\Role'],
+            ['name' => 'pages', 'controller' => 'PageController', 'except' => null, 'only' => null, 'model' => '\\App\\Page'],
+            ['name' => 'services', 'controller' => 'ServiceController', 'except' => null, 'only' => null, 'model' => '\\App\\Service'],
+            ['name' => 'categories', 'controller' => 'CategoryController', 'except' => null, 'only' => null, 'model' => '\\App\\Category'],
+            ['name' => 'posts', 'controller' => 'PostController', 'except' => null, 'only' => null, 'model' => '\\App\\Post'],
+            ['name' => 'tags', 'controller' => 'TagController', 'except' => null, 'only' => null, 'model' => '\\App\\Tag'],
+            ['name' => 'books', 'controller' => 'BookController', 'except' => null, 'only' => null, 'model' => '\\App\\Book'],
+            ['name' => 'likes', 'controller' => 'LikeController', 'except' => null, 'only' => null, 'model' => '\\App\\Like'],
+            ['name' => 'comments', 'controller' => 'CommentController', 'except' => null, 'only' => null, 'model' => '\\App\\Comment'],
+            ['name' => 'faqs', 'controller' => 'FaqController', 'except' => null, 'only' => null, 'model' => '\\App\\Faq'],
+            ['name' => 'countries', 'controller' => 'CountryController', 'except' => null, 'only' => null, 'model' => '\\App\\Country'],
+            ['name' => 'cities', 'controller' => 'CityController', 'except' => null, 'only' => null, 'model' => '\\App\\City'],
+            ['name' => 'towns', 'controller' => 'TownController', 'except' => null, 'only' => null, 'model' => '\\App\\Town'],
+            ['name' => 'menus', 'controller' => 'MenuController', 'except' => null, 'only' => null, 'model' => '\\App\\Menu'],
+            ['name' => 'menu-items', 'controller' => 'MenuController', 'except' => null, 'only' => null, 'model' => '\\App\\MenuItem'],
+            ['name' => 'links', 'controller' => 'LinkController', 'except' => ['create', 'edit'], 'only' => null, 'model' => '\\App\\Link'],
+            ['name' => 'subscribers', 'controller' => 'SubscriberController', 'except' => null, 'only' => null, 'model' => '\\App\\Subscriber'],
+            ['name' => 'sliders', 'controller' => 'SliderController', 'except' => null, 'only' => null, 'model' => '\\App\\Slider'],
+            ['name' => 'banners', 'controller' => 'BannerController', 'except' => null, 'only' => null, 'model' => '\\App\\Banner'],
+            ['name' => 'settings', 'controller' => 'SettingController', 'except' => null, 'only' => null, 'model' => '\\App\\Setting'],
+            ['name' => 'record301s', 'controller' => 'Record301Controller', 'except' => null, 'only' => null, 'model' => '\\App\\Record301'],
+//            ['name' => 'record404s', 'controller' => 'Record404Controller', 'except' => null, 'only' => null, 'model' => '\\App\\Record404'],
+//            ['name' => 'record500s', 'controller' => 'Record500Controller', 'except' => null, 'only' => null, 'model' => '\\App\\Record500'],
+            //            ['name' => 'favorites', 'controller' => 'FavoriteController', 'except' => null, 'only' => null],
+            //            ['name' => 'mails', 'controller' => 'MailController', 'except' => null, 'only' => null],
+            //            ['name' => 'notifications', 'controller' => 'NotificationController', 'except' => null, 'only' => null],
+            //            ['name' => 'notes', 'controller' => 'NoteController', 'except' => null, 'only' => null],
+        ];
+    }
+}
+/*----------------------------------------------------------------------------------*/
